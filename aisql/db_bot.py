82aca1c0238375a1972dff8ad6a960f1992c4e2e
kobycut <kobycut@gmail.com>
2025-01-27 11:14:40 -0700

    hey guys

---
+++ aisql/db_bot.py
@@ -0,0 +1,129 @@
+import json
+from openai import OpenAI
+import os
+import sqlite3
+from time import time
+
+print("Running db_bot.py!")
+
+fdir = os.path.dirname(__file__)
+def getPath(fname):
+    return os.path.join(fdir, fname)
+
+# SQLITE
+sqliteDbPath = getPath("aidb.sqlite")
+setupSqlPath = getPath("setup.sql")
+setupSqlDataPath = getPath("setupData.sql")
+
+# Erase previous db
+if os.path.exists(sqliteDbPath):
+    os.remove(sqliteDbPath) 
+
+sqliteCon = sqlite3.connect(sqliteDbPath) # create new db
+sqliteCursor = sqliteCon.cursor()
+with (
+        open(setupSqlPath) as setupSqlFile,
+        open(setupSqlDataPath) as setupSqlDataFile
+    ):
+
+    setupSqlScript = setupSqlFile.read()
+    setupSQlDataScript = setupSqlDataFile.read()
+
+sqliteCursor.executescript(setupSqlScript) # setup tables and keys
+sqliteCursor.executescript(setupSQlDataScript) # setup tables and keys
+
+def runSql(query):
+    result = sqliteCursor.execute(query).fetchall()
+    return result
+
+# OPENAI
+configPath = getPath("config.json")
+print(configPath)
+with open(configPath) as configFile:
+    config = json.load(configFile)
+
+openAiClient = OpenAI(api_key = config["openaiKey"])
+
+def getChatGptResponse(content):
+    stream = openAiClient.chat.completions.create(
+        model="gpt-4o",
+        messages=[{"role": "user", "content": content}],
+        stream=True,
+    )
+
+    responseList = []
+    for chunk in stream:
+        if chunk.choices[0].delta.content is not None:
+            responseList.append(chunk.choices[0].delta.content)
+
+    result = "".join(responseList)
+    return result
+
+
+# strategies
+commonSqlOnlyRequest = " Give me a sqlite select statement that answers the question. Only respond with sqlite syntax. If there is an error do not expalin it!"
+strategies = {
+    "zero_shot": setupSqlScript + commonSqlOnlyRequest,
+    "single_domain_double_shot": (setupSqlScript + 
+                   " Who doesn't have a way for us to text them? " + 
+                   " \nSELECT p.person_id, p.name\nFROM person p\nLEFT JOIN phone ph ON p.person_id = ph.person_id AND ph.can_recieve_sms = 1\nWHERE ph.phone_id IS NULL;\n " +
+                   commonSqlOnlyRequest)
+}
+
+questions = [
+    "Which shop sells the most cookies?",
+    "Which cookie is sold at the most shops?",
+    "Who's birthday is next?",
+    "Which shop has the most profit?",
+    "Which is the least popular cookie across all shops?",
+    "Which customer has the biggest orders?",
+    "Where does Devin live?",
+        # "I need insert sql into my tables can you provide good unique data?"
+]
+
+def sanitizeForJustSql(value):
+    gptStartSqlMarker = "```sql"
+    gptEndSqlMarker = "```"
+    if gptStartSqlMarker in value:
+        value = value.split(gptStartSqlMarker)[1]
+    if gptEndSqlMarker in value:
+        value = value.split(gptEndSqlMarker)[0]
+
+    return value
+
+for strategy in strategies:
+    responses = {"strategy": strategy, "prompt_prefix": strategies[strategy]}
+    questionResults = []
+    for question in questions:
+        print(question)
+        error = "None"
+        try:
+            sqlSyntaxResponse = getChatGptResponse(strategies[strategy] + " " + question)
+            sqlSyntaxResponse = sanitizeForJustSql(sqlSyntaxResponse)
+            print(sqlSyntaxResponse)
+            queryRawResponse = str(runSql(sqlSyntaxResponse))
+            print(queryRawResponse)
+            friendlyResultsPrompt = "I asked a question \"" + question +"\" and the response was \""+queryRawResponse+"\" Please, just give a concise response in a more friendly way? Please do not give any other suggests or chatter."
+            friendlyResponse = getChatGptResponse(friendlyResultsPrompt)
+            print(friendlyResponse)
+        except Exception as err:
+            error = str(err)
+            print(err)
+
+        questionResults.append({
+            "question": question, 
+            "sql": sqlSyntaxResponse, 
+            "queryRawResponse": queryRawResponse,
+            "friendlyResponse": friendlyResponse,
+            "error": error
+        })
+
+    responses["questionResults"] = questionResults
+
+    with open(getPath(f"response_{strategy}_{time()}.json"), "w") as outFile:
+        json.dump(responses, outFile, indent = 2)
+            
+
+sqliteCursor.close()
+sqliteCon.close()
+print("Done!")
