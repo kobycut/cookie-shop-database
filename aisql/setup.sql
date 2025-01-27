a164d5ee83a2a973296a74f4b3cbb622984ecd93
Joshua Culmer <163606650+joshuaculmer@users.noreply.github.com>
2025-01-27 11:34:16 -0700

    Resyntaxed setup.sql for sqlite

--- aisql/setup.sql
+++ aisql/setup.sql
@@ -1,58 +1,54 @@
-CREATE TABLE `Customer`(
-    `CustomerID` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `FirstName` VARCHAR(255) NOT NULL,
-    `LastName` VARCHAR(255) NOT NULL,
-    `Email` VARCHAR(255) NOT NULL,
-    `Birthday` DATE NOT NULL,
-    `Points` INT NOT NULL
+CREATE TABLE Customer (
+    CustomerID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    FirstName TEXT NOT NULL,
+    LastName TEXT NOT NULL,
+    Email TEXT NOT NULL,
+    Birthday DATE NOT NULL,
+    Points INTEGER NOT NULL
 );
-CREATE TABLE `Order`(
-    `OrderID` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `ShopID` INT NOT NULL,
-    `CustomerID` INT NOT NULL,
-    PRIMARY KEY(`ShopID`)
+
+CREATE TABLE `Order` (
+    OrderID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    ShopID INTEGER NOT NULL,
+    CustomerID INTEGER NOT NULL,
+    FOREIGN KEY (ShopID) REFERENCES Shop (ShopID),
+    FOREIGN KEY (CustomerID) REFERENCES Customer (CustomerID)
 );
-CREATE TABLE `CookieOrder`(
-    `OrderID` INT NOT NULL,
-    `CookieID` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `Amount` INT NOT NULL,
-    PRIMARY KEY(`OrderID`)
+
+CREATE TABLE CookieOrder (
+    OrderID INTEGER NOT NULL,
+    CookieID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    Amount INTEGER NOT NULL,
+    FOREIGN KEY (OrderID) REFERENCES `Order` (OrderID)
 );
-CREATE TABLE `Cookie`(
-    `CookieID` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `Type` CHAR(2) NOT NULL,
-    `CookieName` VARCHAR(255) NOT NULL,
-    `GlutenFree` BOOLEAN NOT NULL,
-    `DairyFree` BOOLEAN NOT NULL
+
+CREATE TABLE Cookie (
+    CookieID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    Type TEXT NOT NULL,
+    CookieName TEXT NOT NULL,
+    GlutenFree BOOLEAN NOT NULL,
+    DairyFree BOOLEAN NOT NULL
 );
-CREATE TABLE `Shop`(
-    `ShopID` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `AddressID` INT NOT NULL,
-    `PhoneNumber` INT NOT NULL,
-    PRIMARY KEY(`AddressID`)
+
+CREATE TABLE Shop (
+    ShopID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    AddressID INTEGER NOT NULL,
+    PhoneNumber TEXT NOT NULL,
+    FOREIGN KEY (AddressID) REFERENCES Address (AddressID)
 );
-CREATE TABLE `CookieShop`(
-    `ShopID` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `CookieID` INT NOT NULL,
-    PRIMARY KEY(`CookieID`)
+
+CREATE TABLE CookieShop (
+    ShopID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    CookieID INTEGER NOT NULL,
+    FOREIGN KEY (CookieID) REFERENCES Cookie (CookieID)
 );
-CREATE TABLE `Address`(
-    `AddressID` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `State` CHAR(2) NOT NULL,
-    `Zip` INT NOT NULL,
-    `City` VARCHAR(255) NOT NULL
+
+CREATE TABLE Address (
+    AddressID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    State TEXT NOT NULL,
+    Zip INTEGER NOT NULL,
+    City TEXT NOT NULL
 );
-ALTER TABLE
-    `Shop` ADD CONSTRAINT `shop_addressid_foreign` FOREIGN KEY(`AddressID`) REFERENCES `Address`(`AddressID`);
-ALTER TABLE
-    `Order` ADD CONSTRAINT `order_orderid_foreign` FOREIGN KEY(`OrderID`) REFERENCES `CookieOrder`(`OrderID`);
-ALTER TABLE
-    `Order` ADD CONSTRAINT `order_customerid_foreign` FOREIGN KEY(`CustomerID`) REFERENCES `Customer`(`CustomerID`);
-ALTER TABLE
-    `Cookie` ADD CONSTRAINT `cookie_cookieid_foreign` FOREIGN KEY(`CookieID`) REFERENCES `CookieShop`(`ShopID`);
-ALTER TABLE
-    `Order` ADD CONSTRAINT `order_shopid_foreign` FOREIGN KEY(`ShopID`) REFERENCES `Shop`(`ShopID`);
-ALTER TABLE
-    `Cookie` ADD CONSTRAINT `cookie_cookieid_foreign` FOREIGN KEY(`CookieID`) REFERENCES `CookieOrder`(`CookieID`);
-ALTER TABLE
-    `Shop` ADD CONSTRAINT `shop_shopid_foreign` FOREIGN KEY(`ShopID`) REFERENCES `CookieShop`(`CookieID`);
+
+-- Add constraints
+PRAGMA foreign_keys = ON;
